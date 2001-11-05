add19283da3fdce799db52ba2648f9c992581bb9
Scott Tringali <tringali@users.sourceforge.net>
2001-11-05 15:17:58 +0000

    Update C/C++ preprocessor patterns with specific keywords (#include, #define, etc.)

--- source/highlightData.c
+++ source/highlightData.c
@@ -1,4 +1,4 @@
-static const char CVSID[] = "$Id: highlightData.c,v 1.24 2001/10/21 15:13:07 tringali Exp $";
+static const char CVSID[] = "$Id: highlightData.c,v 1.25 2001/11/05 15:17:58 tringali Exp $";
 /*******************************************************************************
 *									       *
 * highlightData.c -- Maintain, and allow user to edit, highlight pattern list  *
@@ -240,11 +240,12 @@ static char *DefaultPatternSets[] = {
 	comment:\"/\\*\":\"\\*/\"::Comment::\n\
 	cplus comment:\"//\":\"$\"::Comment::\n\
 	string:\"L?\"\"\":\"\"\"\":\"\\n\":String::\n\
-	preprocessor line:\"^[ \\t]*#\":\"$\"::Preprocessor::\n\
+	preprocessor line:\"^\\s*#\\s*(include|define|if|ifn?def|line|error|else|endif|elif|undef|pragma)>\":\"$\"::Preprocessor::\n\
 	string escape chars:\"\\\\(.|\\n)\":::String1:string:\n\
 	preprocessor esc chars:\"\\\\(.|\\n)\":::Preprocessor1:preprocessor line:\n\
 	preprocessor comment:\"/\\*\":\"\\*/\"::Comment:preprocessor line:\n\
 	preproc cplus comment:\"//\":\"$\"::Comment:preprocessor line:\n\
+	preprocessor keywords:\"<__(LINE|FILE|DATE|TIME|STDC)__>\":::Preprocessor::\n\
 	character constant:\"L?'\":\"'\":\"[^\\\\][^']\":Character Const::\n\
 	numeric constant:\"<((0(x|X)[0-9a-fA-F]*)|(([0-9]+\\.?[0-9]*)|(\\.[0-9]+))((e|E)(\\+|-)?[0-9]+)?)(L|l|UL|ul|u|U|F|f)?>\":::Numeric Const::D\n\
 	storage keyword:\"<(class|typename|typeid|template|friend|virtual|inline|explicit|operator|public|private|protected|const|extern|auto|register|static|mutable|unsigned|signed|volatile|char|double|float|int|long|short|bool|wchar_t|void|typedef|struct|union|enum|asm|export)>\":::Storage Type::D\n\
@@ -253,10 +254,11 @@ static char *DefaultPatternSets[] = {
     "C:1:0 {\n\
     	comment:\"/\\*\":\"\\*/\"::Comment::\n\
 	string:\"L?\"\"\":\"\"\"\":\"\\n\":String::\n\
-    	preprocessor line:\"^[ \\t]*#\":\"$\"::Preprocessor::\n\
+	preprocessor line:\"^\\s*#\\s*(include|define|if|ifn?def|line|error|else|endif|elif|undef|pragma)>\":\"$\"::Preprocessor::\n\
     	string escape chars:\"\\\\(.|\\n)\":::String1:string:\n\
     	preprocessor esc chars:\"\\\\(.|\\n)\":::Preprocessor1:preprocessor line:\n\
     	preprocessor comment:\"/\\*\":\"\\*/\"::Comment:preprocessor line:\n\
+	preprocessor keywords:\"<__(LINE|FILE|DATE|TIME|STDC)__>\":::Preprocessor::\n\
 	character constant:\"L?'\":\"'\":\"[^\\\\][^']\":Character Const::\n\
 	numeric constant:\"<((0(x|X)[0-9a-fA-F]*)|(([0-9]+\\.?[0-9]*)|(\\.[0-9]+))((e|E)(\\+|-)?[0-9]+)?)(L|l|UL|ul|u|U|F|f)?>\":::Numeric Const::D\n\
     	storage keyword:\"<(const|extern|auto|register|static|unsigned|signed|volatile|char|double|float|int|long|short|void|typedef|struct|union|enum)>\":::Storage Type::D\n\
@@ -457,7 +459,7 @@ static char *DefaultPatternSets[] = {
 	comment:\"/\\*\":\"\\*/\"::Comment::\n\
 	string:\"\"\"\":\"\"\"\":\"\\n\":String::\n\
 	meta string:\"\\\\\"\".*\\\\\"\"\":::String::\n\
-	preprocessor line:\"^[ t]*#\":\"$\"::Preprocessor::\n\
+	preprocessor line:\"^\\s*#\\s*(include|define|if|ifn?def|line|error|else|endif|elif|undef|pragma)>\":\"$\"::Preprocessor::\n\
 	string escape chars:\"\\\\(.|\\n)\":::String1:string:\n\
 	preprocessor esc chars:\"\\\\(.|\\n)\":::Preprocessor1:preprocessor line:\n\
 	preprocessor comment:\"/\\*\":\"\\*/\"::Comment:preprocessor line:\n\
@@ -939,7 +941,7 @@ static char *DefaultPatternSets[] = {
     "Yacc:1:0{\n\
 	comment:\"/\\*\":\"\\*/\"::Comment::\n\
 	string:\"\"\"\":\"\"\"\":\"\\n\":String::\n\
-	preprocessor line:\"^[ 	]*#\":\"$\"::Preprocessor::\n\
+	preprocessor line:\"^\\s*#\\s*(include|define|if|ifn?def|line|error|else|endif|elif|undef|pragma)>\":\"$\"::Preprocessor::\n\
 	string escape chars:\"\\\\(.|\\n)\":::String1:string:\n\
 	preprocessor esc chars:\"\\\\(.|\\n)\":::Preprocessor1:preprocessor line:\n\
 	preprocessor comment:\"/\\*\":\"\\*/\"::Comment:preprocessor line:\n\
