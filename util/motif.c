b61a598d0d851c07431305acbacec9e9c3c4da02
Scott Tringali <tringali@users.sourceforge.net>
2006-10-16 19:52:32 +0000

    0.95.0 has the menu unposting bug, too.  Verified with LessTif's test suite.

--- util/motif.c
+++ util/motif.c
@@ -90,6 +90,7 @@ const char *const knownBadLessTif[] = {
     "0.93.44",
     "0.93.95b",   /* SF bug 1087192 */
     "0.94.4",     /* Alt-H, ESC => crash */
+    "0.95.0",     /* same as above */
     NULL
 };
 
